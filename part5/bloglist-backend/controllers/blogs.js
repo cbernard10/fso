5cda815049b9349bccf3e9470a886c57b8984bef
cbernard10 <112329508+cbernard10@users.noreply.github.com>
2023-12-07 12:42:29 +0100

    Add files via upload

---
+++ part5/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,102 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const middleware = require("../utils/middleware");
+
+const jwt = require("jsonwebtoken");
+
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.status(200).json(blogs);
+});
+
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  response.status(200).json(blog);
+});
+
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
+  const blog = new Blog(request.body);
+  const creator = await request.user;
+  blog.user = creator._id;
+
+  if (blog.likes === undefined) {
+    blog["likes"] = 0;
+  }
+
+  if (
+    blog.title === undefined ||
+    blog.url === undefined ||
+    blog.title === "" ||
+    blog.url === ""
+  ) {
+    response.status(400).end();
+  } else {
+    const savedBlog = await blog.save();
+
+    creator.blogs = creator.blogs.concat(savedBlog._id);
+    await creator.save();
+
+    Blog.findById(savedBlog._id)
+      .populate("user", { username: 1, name: 1 })
+      .then((savedBlog) => {
+        response.status(201).json(savedBlog);
+      })
+      .catch(error => next(error))
+
+    // response.status(201).json(savedBlog);
+  }
+});
+
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const token = request.token;
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    if (!decodedToken.id)
+      return response
+        .status(401)
+        .json({ error: "invalid token, cannot delete blog" });
+
+    const blog = await Blog.findById(request.params.id);
+
+    const creator = blog.user.toString();
+    const user = await request.user;
+    const userId = user._id.toString();
+
+    if (creator !== userId) {
+      return response
+        .status(401)
+        .json({ error: "invalid user, cannot delete blog" });
+    } else {
+      try {
+        await Blog.findByIdAndDelete(request.params.id);
+        response.status(204).end();
+      } catch (error) {
+        return response.status(400).json({ error: "could not delete blog" });
+      }
+    }
+  }
+);
+
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: body.user,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .populate("user", { username: 1, name: 1 })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
---
+++ part5/bloglist-backend/controllers/users.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  if (!username || !password) {
+    response.statusMessage = "Username or password missing";
+    return response.status(400).end();
+  } else if (username.length < 3) {
+    response.statusMessage = "Username must be at least 3 characters long";
+    return response.status(400).end();
+  } else if (password.length < 3) {
+    response.statusMessage = "Password must be at least 3 characters long";
+    return response.status(400).end();
+  } else {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    response.status(201).json(savedUser);
+  }
+});
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.get("/:id", async (request, response) => {
+  const user = await User.findById(request.params.id);
+  response.json(user);
+})
+
+module.exports = usersRouter;
