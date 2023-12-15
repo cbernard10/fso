d1d5707008dbda97baee1bb2f8e2da27874a5c3e
cbernard10 <112329508+cbernard10@users.noreply.github.com>
2023-12-15 18:40:12 +0100

    Add files via upload

---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,129 @@
+import Footer from "./Footer";
+import { useState } from "react";
+import AnecdoteForm from "./AnecdoteForm";
+import About from "./About";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch,
+} from "react-router-dom";
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5,
+  };
+  return (
+    <div>
+      <Link to="/" style={padding}>
+        anecdotes
+      </Link>
+      <Link to="/create" style={padding}>
+        create new
+      </Link>
+      <Link to="/about" style={padding}>
+        about
+      </Link>
+    </div>
+  );
+};
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map((anecdote) => (
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+
+const Anecdote = ({ anecdote }) => {
+  console.log(anecdote);
+  return (
+    <div>
+      <h2>
+        {anecdote.content} by {anecdote.author}
+      </h2>
+      <p>has {anecdote.votes} votes</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: 1,
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: 2,
+    },
+  ]);
+
+  const [notification, setNotification] = useState("");
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000);
+    setAnecdotes(anecdotes.concat(anecdote));
+    setNotification(`a new anecdote ${anecdote.content} created!`);
+    setTimeout(() => setNotification(""), 5000);
+  };
+
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id);
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    };
+
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  };
+
+  const match = useMatch("/anecdotes/:id");
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+    : null;
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <p style={{fontWeight: 600}}>{notification}</p>
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<AnecdoteForm addNew={addNew} />} />
+        <Route
+          path="/anecdotes/:id"
+          element={<Anecdote anecdote={anecdote} />}
+        />
+      </Routes>
+      {/* <AnecdoteList anecdotes={anecdotes} />
+      <About />
+      <CreateNew addNew={addNew} /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
